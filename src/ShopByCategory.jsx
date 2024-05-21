f568c99219f88abb6434656d8b23c0f70e7751f1
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-05-21 14:10:51 +0600

    Sub Category show and functionality problem issue

--- src/ShopByCategory.jsx
+++ src/ShopByCategory.jsx
@@ -3,10 +3,10 @@ import React, { useEffect, useState } from 'react'
 import { FaMinus, FaPlus } from 'react-icons/fa'
 
 export default function ShopByCategory() {
-  const [expandedCategory, setExpandedCategory] = useState(null);
+  const [ToggleCategory,setToggleCategory] = useState(false);
   const [fetchCategory , setfetchCategory] = useState([]);
 
-  useEffect(() => {
+   useEffect(() => {
     const fetchGetCategory = async () => {
       try {
         const response = await axios.get("http://localhost:7000/api/v1/allget/getAllCategory");
@@ -17,38 +17,23 @@ export default function ShopByCategory() {
     }
     fetchGetCategory();
   }, []);
-
-  const handleToggle = (index) => {
-    if (expandedCategory === index) {
-      setExpandedCategory(null);
-    } else {
-      setExpandedCategory(index);
-    }
-  };
-
   return (
     <>
-      {/* Shop by Category */}
-      <h3 className='font-semibold text-xl mt-5'>Shop by Category</h3>
-      {
-        fetchCategory.map((item, index) => (
-          <div key={index}>
-            <button onClick={() => handleToggle(index)} className="flex justify-between items-center w-full py-2">
-              <p className='font-bold text-green-600'>{item.name}</p>
-              {expandedCategory === index ? <FaMinus size={15}/> : <FaPlus size={15}/>}
-            </button>
-           {item.subCategory.length >0 ?
-           (
-            <p>ace</p>
-           ) 
-           :
-           (
-            <p>nai</p>
-           )
-          }
-          </div>
-        ))
-      }
+        {/*Shop by Category  */}
+        <h3 className='font-semibold text-xl  mt-5'>Shop by Category</h3>
+            {
+              fetchCategory.map((item,index)=>{
+               return <>
+               <button key={index} onClick={()=>setToggleCategory(!ToggleCategory)} className="flex justify-between items-center">
+                <p className=' font-bold text-green-600'>{item.name}</p>
+                {ToggleCategory ?<FaMinus size={15}/> :<FaPlus size={15}/>}
+                </button>
+                {item.subCategory?.map((item)=>ToggleCategory && <p className=' border-b text-sm text-re'>{item.name}</p>)}
+               </>
+              })
+            }
+          
+           
     </>
   )
 }
