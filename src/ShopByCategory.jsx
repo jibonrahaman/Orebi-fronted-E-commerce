e752a257a9d69b0c366d7c573e65363afd364cec
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-05-21 14:10:26 +0600

    Sub Category show and functionality problem issue

--- src/ShopByCategory.jsx
+++ src/ShopByCategory.jsx
@@ -1,20 +1,48 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { FaMinus, FaPlus } from 'react-icons/fa'
 
 export default function ShopByCategory() {
-  const [ToggleCategory,setToggleCategory] = useState(false)
-  
+  const [expandedCategory, setExpandedCategory] = useState(null);
+  const [fetchCategory , setfetchCategory] = useState([]);
+
+  useEffect(() => {
+    const fetchGetCategory = async () => {
+      try {
+        const response = await axios.get("http://localhost:7000/api/v1/allget/getAllCategory");
+        setfetchCategory(response.data.CATEGORYS);
+      } catch (error) {
+        console.log(error, "CategoryStatus Page-");
+      }
+    }
+    fetchGetCategory();
+  }, []);
+
+  const handleToggle = (index) => {
+    if (expandedCategory === index) {
+      setExpandedCategory(null);
+    } else {
+      setExpandedCategory(index);
+    }
+  };
+
   return (
     <>
-        {/*Shop by Category  */}
-        <h3 className='font-semibold text-xl  mt-5'>Shop by Category</h3>
-            <button onClick={()=>setToggleCategory(!ToggleCategory)} className="flex justify-between items-center">
-            <p>Category 1</p>
-            {ToggleCategory ?<FaMinus size={15}/> :<FaPlus size={15}/>}
+      {/* Shop by Category */}
+      <h3 className='font-semibold text-xl mt-5'>Shop by Category</h3>
+      {
+        fetchCategory.map((item, index) => (
+          <div key={index}>
+            <button onClick={() => handleToggle(index)} className="flex justify-between items-center w-full py-2">
+              <p className='font-bold text-green-600'>{item.name}</p>
+              {expandedCategory === index ? <FaMinus size={15}/> : <FaPlus size={15}/>}
             </button>
-            {ToggleCategory && <p className=' border-b'>demo</p>}
-          
-           
+            {item.subCategory?.map((subItem, subIndex) => (
+             expandedCategory === index && <p key={subIndex} className='border-b text-sm text-red-700 ml-4'>{subItem.name}</p>
+            ))}
+          </div>
+        ))
+      }
     </>
   )
 }
